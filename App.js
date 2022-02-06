9932544ef25021de025f65d7e50820f6372789fd
Lorenzo Orders <ordelore@gmail.com>
2022-02-06 13:04:28 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-export default function App() {
+export default function App () {
+  const onPressIn = (evt) => {console.log(`clicked in at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);};
+  const onPressOut = (evt) => {console.log(`clicked out at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);};
   return (
     <View style={styles.container}>
+      <Pressable onPressIn={onPressIn} onPressOut={onPressOut} >
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      </Pressable>
     </View>
   );
 }
