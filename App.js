ddb06ca6f17ce9b1aac67c20dbff0ff8fb8dff02
Lorenzo Orders <ordelore@gmail.com>
2022-02-21 15:05:26 -0600

    leaves trails

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Canvas from 'react-native-canvas';
 export default class App extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {reset: false};
     this.tapIn = [0, 0, 0],
       this.tapOut = [0, 0, 0],
       this.canvas = React.createRef(),
@@ -15,7 +15,7 @@ export default class App extends Component {
     this.onPressOut = this.onPressOut.bind(this);
     this.drawBalls = this.drawBalls.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
+    // this.componentDidUpdate = this.componentDidUpdate.bind(this);
     this.previousFrame = (new Date()).getTime();
 
   }
@@ -25,23 +25,22 @@ export default class App extends Component {
     canvas.height = Dimensions.get('window').height;
     this.drawBalls();
   }
-  componentDidUpdate() {
-    this.drawBalls();
-  }
 
   drawBalls() {
     const { balls } = this.balls;
     const canvas = this.canvas.current;
-    const context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    const context = canvas.getContext("2d", {alpha: false});
+    context.width = canvas.width;
+    context.height = canvas.height;
+
     const currentFrame = (new Date()).getTime();
     balls.forEach(ball => {
       context.fillStyle = ball.color;
       context.beginPath();
-      context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+      context.arc(Math.floor(ball.x), Math.floor(ball.y), ball.radius, 0, 2 * Math.PI);
       context.closePath();
       context.fill();
-      ball.step(canvas, 1 * (currentFrame - this.previousFrame));
+      ball.step(canvas, currentFrame - this.previousFrame);
     });
     this.previousFrame = currentFrame;
     requestAnimationFrame(this.drawBalls);
@@ -54,6 +53,7 @@ export default class App extends Component {
       console.log(`clicked in at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);
       this.tapIn = [evt.nativeEvent.locationX, evt.nativeEvent.locationY, evt.nativeEvent.timestamp];
     }
+    this.setState({reset: !this.state.reset});
   };
   onPressOut(evt) {
     if (this.tapIn[2] !== 0) {
@@ -64,6 +64,8 @@ export default class App extends Component {
       this.balls.addBall(this.tapIn[0], this.tapIn[1], -change[0] / change[2], -change[1] / change[2]);
       this.tapIn = [0, 0, 0];
     }
+    this.setState({reset: !this.state.reset});
+
   };
 
   render() {
