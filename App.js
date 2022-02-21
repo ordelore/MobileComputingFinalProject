3fbf874b1383260a6610b80e85fd4f2ae4fcdf55
Lorenzo Orders <ordelore@gmail.com>
2022-02-20 23:52:29 -0600

    bouncing balls

--- App.js
+++ App.js
@@ -1,22 +1,77 @@
 import React, { useState, useEffect, createRef, Component } from "react";
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, Dimensions } from 'react-native';
+import Balls from './Balls';
 import Canvas from 'react-native-canvas';
 
 export default class App extends Component {
-  handleCanvas(canvas) {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
+  constructor(props) {
+    super(props);
+
+    this.tapIn = [0, 0, 0],
+      this.tapOut = [0, 0, 0],
+      this.canvas = React.createRef(),
+      this.balls = new Balls();
+    this.onPressIn = this.onPressIn.bind(this);
+    this.onPressOut = this.onPressOut.bind(this);
+    this.drawBalls = this.drawBalls.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentDidUpdate = this.componentDidUpdate.bind(this);
+    this.previousFrame = (new Date()).getTime();
+
+  }
+  componentDidMount() {
+    const canvas = this.canvas.current;
+    canvas.width = Dimensions.get('window').width;
+    canvas.height = Dimensions.get('window').height;
+    this.drawBalls();
+  }
+  componentDidUpdate() {
+    this.drawBalls();
+  }
+
+  drawBalls() {
+    const { balls } = this.balls;
+    const canvas = this.canvas.current;
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const currentFrame = (new Date()).getTime();
+    balls.forEach(ball => {
+      context.fillStyle = ball.color;
+      context.beginPath();
+      context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+      context.closePath();
+      context.fill();
+      ball.step(canvas, 1 * (currentFrame - this.previousFrame));
+    });
+    this.previousFrame = currentFrame;
+    requestAnimationFrame(this.drawBalls);
+  }
+
+  onPressIn(evt) {
+    if (this.tapIn[2] === 0) {
+      this.tapIn = [0, 0, 0];
+      this.tapOut = [0, 0, 0];
+      console.log(`clicked in at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);
+      this.tapIn = [evt.nativeEvent.locationX, evt.nativeEvent.locationY, evt.nativeEvent.timestamp];
+    }
   };
-  onPressIn(evt) {console.log(`clicked in at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);};
-  onPressOut(evt) {console.log(`clicked out at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);};
+  onPressOut(evt) {
+    if (this.tapIn[2] !== 0) {
+      console.log(`clicked out at (${evt.nativeEvent.locationX}, ${evt.nativeEvent.locationY}) at time ${evt.nativeEvent.timestamp}`);
+      this.tapOut = [evt.nativeEvent.locationX, evt.nativeEvent.locationY, evt.nativeEvent.timestamp];
+      var change = this.tapOut.map((x, i) => x - this.tapIn[i]);
+      console.log(`velocity: ${change[0] / change[2]}, ${change[1] / change[2]}`);
+      this.balls.addBall(this.tapIn[0], this.tapIn[1], -change[0] / change[2], -change[1] / change[2]);
+      this.tapIn = [0, 0, 0];
+    }
+  };
+
   render() {
+
     return (
       <View style={styles.container}>
         <Pressable onPressIn={this.onPressIn} onPressOut={this.onPressOut} >
-        <StatusBar style="auto" />
-        <Canvas ref={this.handleCanvas}/>
+          <Canvas ref={this.canvas} />
         </Pressable>
       </View>
     );
---
+++ Balls.js
@@ -0,0 +1,70 @@
+export default class Balls {
+    constructor() {
+        this.balls = [];
+    }
+    addBall(x, y, dx, dy) {
+        this.balls.push(new Ball(x, y, 5, "red", dx, dy));
+    }
+    updateBalls() {
+        this.balls.forEach(ball => {
+            ball.step();
+        });
+    }
+    getBalls() {
+        return this.balls;
+    }
+}
+class Ball {
+    constructor(x, y, r, color, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.radius = r;
+        this.dx = dx;
+        this.dy = dy;
+        this.toRight = dx >> 0.0;
+        this.toBottom = dy << 0.0;
+        this.iteration = 0;
+    }
+    doBounceIfNeeded(canvas) {
+        let rightTouch, bottomTouch, leftTouch, topTouch;
+        rightTouch = this.x >= canvas.width - this.radius;
+        bottomTouch = this.y >= canvas.height - this.radius;
+        leftTouch = this.x <= this.radius;
+        topTouch = this.y <= this.radius;
+
+        if (rightTouch || leftTouch) {
+            this.toRight = !this.toRight;
+        }
+        if (bottomTouch || topTouch) {
+            this.toBottom = !this.toBottom;
+        }
+    }
+    step(canvas, deltaTime) {
+        const safeDistance = this.radius * 10;
+        if (this.toRight && this.toBottom) {
+            this.x += this.dx * deltaTime;
+            this.y += this.dy * deltaTime;
+            this.iteration++;
+        } else if (!this.toRight && this.toBottom) {
+            this.x -= this.dx * deltaTime;
+            this.y += this.dy * deltaTime;
+            this.iteration++;
+
+        } else if (!this.toRight && !this.toBottom) {
+            this.x -= this.dx * deltaTime;
+            this.y -= this.dy * deltaTime;
+            this.iteration++;
+        } else if (this.toRight && !this.toBottom) {
+            this.x += this.dx * deltaTime;
+            this.y -= this.dy * deltaTime;
+            this.iteration++;
+        }
+        if (
+            this.iteration >= safeDistance / this.dy - this.radius ||
+            this.iteration >= safeDistance / this.dx - this.radius
+        ) {
+            this.doBounceIfNeeded(canvas);
+        }
+    }
+}
