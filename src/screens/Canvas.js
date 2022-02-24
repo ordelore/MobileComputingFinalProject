1b715ed12f01449bb3ebd81765099d9cdc4cb5c4
lnealer <lrosenealer@gmail.com>
2022-02-24 12:16:51 -0600

    merge bug fix and clean

--- src/screens/Canvas.js
+++ src/screens/Canvas.js
@@ -2,70 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, Dimensions } from 'react-native';
 import Canvas from 'react-native-canvas';
 import React, {Component, useState, useRef, useEffect} from 'react';
-import Balls from './Balls';
+import Balls from '../Balls';
 import io from "socket.io-client";
 import { render } from "react-dom";
 
-//const Cvs = ({ route }) => {
 export default class Cvs extends Component {
-  // // server refs
-  // const socketRef = useRef();
-  // const otherUser = useRef();
-  // const sendChannel = useRef(); // Data channel
-  // const { roomID } = route.params;
-  // const userID = useRef()
-  // const canvas = useRef(null);
-  // let numUsers = 1
-
-  // canvas refs
-  // state = {reset: false};
-  // let tapIn = [0, 0, 0];
-  // let tapOut = [0, 0, 0];
-  // let balls = new Balls();
-  // let onPressIn = this.onPressIn.bind(this);
-  // let onPressOut = this.onPressOut.bind(this);
-  // let drawBalls = this.drawBalls.bind(this);
-  // //let componentDidMount = componentDidMount.bind(this);
-  // let previousFrame = (new Date()).getTime();
-
-  // useEffect(() => {
-  //   // socket init
-  //   // Step 1: Connect with the Signal server [set your ip address]
-  //   socketRef.current = io.connect("http://10.0.0.53:9000"); // Address of the Signal server
-
-  //   // Step 2: Join the room. If initiator we will create a new room otherwise we will join a room
-  //   socketRef.current.emit("join room", roomID); // Room ID
-
-  //   // Step 3: Waiting for the other peer to join the room
-  //   socketRef.current.on("other user", userID => {
-  //     console.log('other user joined')
-  //     otherUser.current = userID;
-  //     numUsers += 1;
-  //   });
-
-  //   // get your user id 
-  //   socketRef.current.on("userID", user => {
-  //     console.log('set user id', user)
-  //     userID.current = user;
-  //   })
-
-  //   // Signals that both peers have joined the room
-  //   socketRef.current.on("user joined", userID => {
-  //     otherUser.current = userID;
-  //     numUsers += 1;
-  //   });
-
-  //   socketRef.current.on("receiving message", msg => {
-  //     handleReceiveMessage(msg)
-  //   })
-
-  //   // canvas init
-  //   const canvas = this.canvas.current;
-  //   canvas.width = Dimensions.get('window').width;
-  //   canvas.height = Dimensions.get('window').height;
-  //   this.drawBalls();
-
-  // }, []);
 
   constructor(props) {
     super(props);
@@ -83,7 +24,8 @@ export default class Cvs extends Component {
       // server refs
     this.socketRef;
     this.sendChannel; // Data channel
-    this.roomID = route.params;
+    console.log(props.route.params.roomID)
+    this.roomID = props.route.params.roomID;
     this.userID;
   };
 
@@ -98,7 +40,7 @@ export default class Cvs extends Component {
     this.socketRef = io.connect("http://10.0.0.53:9000"); // Address of the Signal server
 
     // Step 2: Join the room. If initiator we will create a new room otherwise we will join a room
-    this.socketRef.emit("join room", roomID); // Room ID
+    this.socketRef.emit("join room", this.roomID); // Room ID
 
     // Step 3: Waiting for the other peer to join the room
     this.socketRef.on("other user", userID => {
